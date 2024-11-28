c4bf05e50c82f948385f7774e1cfc924ac90e0ef
maulidaprina <mldaprna@gmail.com>
2024-11-29 01:05:56 +0700

    Tambahkan kode program untuk mencetak deret perhitungan pangkatnya.

--- Percobaan2_12.java
+++ Percobaan2_12.java
@@ -1,24 +1,33 @@
 import java.util.Scanner;
+
 public class Percobaan2_12 {
 
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return (1);
+            System.out.print("1");  
+            return 1;
+        } else if (y == 1) {
+            System.out.print(x);  
+            return x;
         } else {
-            return (x * hitungPangkat(x, y - 1));
+            System.out.print(x + " x ");  
+            return x * hitungPangkat(x, y - 1); 
         }
     }
+
     public static void main(String[] args) {
         Scanner input12 = new Scanner(System.in);
-            int bilangan, pangkat;
-        
+        int bilangan, pangkat, hasil;
+
         System.out.print("Bilangan yang dihitung: ");
-        bilangan =  input12.nextInt();
-        
+        bilangan = input12.nextInt();
+
         System.out.print("Pangkat: ");
         pangkat = input12.nextInt();
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        System.out.print("Hasil perhitungan: ");
+        hasil = hitungPangkat(bilangan, pangkat); 
+        System.out.print(" x 1");  
+        System.out.println(" = " + hasil);  
     }
 }
-
